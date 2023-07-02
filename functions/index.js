3f67a2349da55fe42c4f5519a39fe1d8918d3bfe
Samuel Montambault <amos.samuel@hotmail.com>
2023-07-02 16:11:50 -0400

    create 3 endpoints for each actions

--- functions/index.js
+++ functions/index.js
@@ -9,11 +9,65 @@
 
 const {onRequest} = require("firebase-functions/v2/https");
 const logger = require("firebase-functions/logger");
+const {
+  SNSClient,
+  CreatePlatformEndpointCommand,
+  GetEndpointAttributesCommand,
+  SetEndpointAttributesCommand,
+} = require("@aws-sdk/client-sns");
+
 
 // Create and deploy your first functions
 // https://firebase.google.com/docs/functions/get-started
 
-exports.createOrUpdateEndpoint = onRequest((request, response) => {
-  logger.info("Hello logs!", {structuredData: true});
-  response.send(`Hello with keys`);
+exports.createPlatformEndpoint = onRequest(async (request, response) => {
+  logger.info("[createPlatformEndpoint]", {structuredData: true});
+  const region = request.query.region;
+  const token = request.query.token;
+  const client = new SNSClient({region: region, credentials: {
+    accessKeyId: process.env.AWS_ACCESS_KEY,
+    secretAccessKey: process.env.AWS_SECRET_KEY}});
+  const command = new CreatePlatformEndpointCommand({
+    PlatformApplicationArn: process.env.PLATFORM_APPLICATION_ARN,
+    Token: token,
+  });
+  response.send(await client.send(command));
 });
+
+
+exports.getEndpointAttributes = onRequest(async (request, response) => {
+  logger.info("[getEndpointAttributes]", {structuredData: true});
+
+  const region = request.query.region;
+  const endpointArn = request.query.endpointArn;
+  const client = new SNSClient({region: region, credentials: {
+    accessKeyId: process.env.AWS_ACCESS_KEY,
+    secretAccessKey: process.env.AWS_SECRET_KEY}});
+  const command = new GetEndpointAttributesCommand({
+    EndpointArn: endpointArn,
+  });
+  response.send(await client.send(command));
+});
+
+
+exports.setEndpointAttributes = onRequest(async (request, response) => {
+  logger.info("[setEndpointAttributes]", {structuredData: true});
+
+  const region = request.query.region;
+  const endpointArn = request.query.endpointArn;
+  const client = new SNSClient({region: region, credentials: {
+    accessKeyId: process.env.AWS_ACCESS_KEY,
+    secretAccessKey: process.env.AWS_SECRET_KEY}});
+
+  const command = new SetEndpointAttributesCommand({
+    EndpointArn: endpointArn,
+    Attributes: {
+      Token: request.query.token,
+      Enabled: "true",
+    },
+  });
+  response.send(await client.send(command));
+});
+
+
+// https://docs.aws.amazon.com/sns/latest/dg/mobile-platform-endpoint.html
