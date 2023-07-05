939d445ead5e341fad9de3401f08250589d4c71b
MysticFragilist <mont.samuel@outlook.com>
2023-07-05 13:17:31 -0400

    add secret handling

--- functions/index.js
+++ functions/index.js
@@ -20,51 +20,57 @@ const {
 // Create and deploy your first functions
 // https://firebase.google.com/docs/functions/get-started
 
-exports.createPlatformEndpoint = onRequest(async (request, response) => {
-  logger.info("[createPlatformEndpoint]", {structuredData: true});
-  const region = request.query.region;
-  const token = request.query.token;
-  const client = new SNSClient({region: region, credentials: {
-    accessKeyId: process.env.AWS_ACCESS_KEY,
-    secretAccessKey: process.env.AWS_SECRET_KEY}});
-  const command = new CreatePlatformEndpointCommand({
-    PlatformApplicationArn: process.env.AWS_PLATFORM_ARN,
-    Token: token,
-  });
-  response.send(await client.send(command));
-});
+exports.createPlatformEndpoint = onRequest(
+    {secrets: ["AWS_ACCESS_KEY", "AWS_SECRET_KEY", "AWS_PLATFORM_ARN"]},
+    async (request, response) => {
+      logger.info("[createPlatformEndpoint]", {structuredData: true});
+      const region = request.query.region;
+      const token = request.query.token;
+      const client = new SNSClient({region: region, credentials: {
+        accessKeyId: process.env.AWS_ACCESS_KEY,
+        secretAccessKey: process.env.AWS_SECRET_KEY}});
+      const command = new CreatePlatformEndpointCommand({
+        PlatformApplicationArn: process.env.AWS_PLATFORM_ARN,
+        Token: token,
+      });
+      response.send(await client.send(command));
+    });
 
 
-exports.getEndpointAttributes = onRequest(async (request, response) => {
-  logger.info("[getEndpointAttributes]", {structuredData: true});
+exports.getEndpointAttributes = onRequest(
+    {secrets: ["AWS_ACCESS_KEY", "AWS_SECRET_KEY"]},
+    async (request, response) => {
+      logger.info("[getEndpointAttributes]", {structuredData: true});
 
-  const region = request.query.region;
-  const endpointArn = request.query.endpointArn;
-  const client = new SNSClient({region: region, credentials: {
-    accessKeyId: process.env.AWS_ACCESS_KEY,
-    secretAccessKey: process.env.AWS_SECRET_KEY}});
-  const command = new GetEndpointAttributesCommand({
-    EndpointArn: endpointArn,
-  });
-  response.send(await client.send(command));
-});
+      const region = request.query.region;
+      const endpointArn = request.query.endpointArn;
+      const client = new SNSClient({region: region, credentials: {
+        accessKeyId: process.env.AWS_ACCESS_KEY,
+        secretAccessKey: process.env.AWS_SECRET_KEY}});
+      const command = new GetEndpointAttributesCommand({
+        EndpointArn: endpointArn,
+      });
+      response.send(await client.send(command));
+    });
 
 
-exports.setEndpointAttributes = onRequest(async (request, response) => {
-  logger.info("[setEndpointAttributes]", {structuredData: true});
+exports.setEndpointAttributes = onRequest(
+    {secrets: ["AWS_ACCESS_KEY", "AWS_SECRET_KEY"]},
+    async (request, response) => {
+      logger.info("[setEndpointAttributes]", {structuredData: true});
 
-  const region = request.query.region;
-  const endpointArn = request.query.endpointArn;
-  const client = new SNSClient({region: region, credentials: {
-    accessKeyId: process.env.AWS_ACCESS_KEY,
-    secretAccessKey: process.env.AWS_SECRET_KEY}});
+      const region = request.query.region;
+      const endpointArn = request.query.endpointArn;
+      const client = new SNSClient({region: region, credentials: {
+        accessKeyId: process.env.AWS_ACCESS_KEY,
+        secretAccessKey: process.env.AWS_SECRET_KEY}});
 
-  const command = new SetEndpointAttributesCommand({
-    EndpointArn: endpointArn,
-    Attributes: {
-      Token: request.query.token,
-      Enabled: "true",
-    },
-  });
-  response.send(await client.send(command));
-});
+      const command = new SetEndpointAttributesCommand({
+        EndpointArn: endpointArn,
+        Attributes: {
+          Token: request.query.token,
+          Enabled: "true",
+        },
+      });
+      response.send(await client.send(command));
+    });
